e6f5139b1ae243b4f9403cf301d5433476834841
Gvozdvadim <vadimgvozd416@gmail.com>
2018-03-15 14:29:33 +0200

    5555
    6566

--- js/common.js
+++ js/common.js
@@ -1,48 +1,18 @@
-$(document).ready(function() {
-    enableSelectBoxes();
-});
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
-function enableSelectBoxes(){
-    $('div.selectBox').each(function(){
-        $(this).children('span.selected').html($(this).children('div.selectOptions').children('span.selectOption:first').html());
-        $(this).attr('value',$(this).children('div.selectOptions').children('span.selectOption:first').attr('value'));
 
-        $(this).children('span.selected,span.selectArrow').click(function(){
-            if($(this).parent().children('div.selectOptions').css('display') == 'none'){
-                $(this).parent().children('div.selectOptions').css('display','block');
-            }
-            else
-            {
-                $(this).parent().children('div.selectOptions').css('display','none');
-            }
-        });
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-        $(this).find('span.selectOption').click(function(){
-            $(this).parent().css('display','none');
-            $(this).closest('div.selectBox').attr('value',$(this).attr('value'));
-            $(this).parent().siblings('span.selected').html($(this).html());
-        });
-    });				
-}//-->
-$(document).ready(function(){
-    $('body').append('<a href="#" id="go-top" title="Вверх"></a>');
-});
-
-$(function() {
-    $.fn.scrollToTop = function() {
-        $(this).hide().removeAttr("href");
-        if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
-        var scrollDiv = $(this);
-        $(window).scroll(function() {
-            if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-            else $(scrollDiv).fadeIn("slow")
-                });
-        $(this).click(function() {
-            $("html, body").animate({scrollTop: 0}, "slow")
-        })
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-});
+}
 
-$(function() {
-    $("#go-top").scrollToTop();
-});
